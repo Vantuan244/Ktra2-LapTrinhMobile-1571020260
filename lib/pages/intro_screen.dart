9cacd8ccb528d018543703514992567574ca5166
Pigeonchill <94815802+Pigeonchill@users.noreply.github.com>
2024-04-04 09:44:32 +0700

    1571020260

---
+++ lib/pages/intro_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: SafeArea(
+        child: Column(
+          children: [
+            // big logo
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 100.0,
+                right: 100.0,
+                top: 120,
+                bottom: 20,
+              ),
+              child: Image.asset('lib/images/orange.png'),
+            ),
+
+            // we deliver groceries at your doorstep
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: Text(
+                'Xin chào quý khách',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                ),),
+            ),
+
+            // groceree gives you fresh vegetables and fruits
+            Text(
+              'Đơn hàng mỗi ngày',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[700],
+              ),
+            ),
+
+            const SizedBox(height: 24),
+
+            const Spacer(),
+
+            // get started button
+            GestureDetector(
+              onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return HomePage();
+                  },
+                ),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: const Color.fromARGB(255, 112, 91, 222),
+                ),
+                child: const Text(
+                  "Bắt đầu",
+                  style: TextStyle(
+                    color: Colors.white,
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
